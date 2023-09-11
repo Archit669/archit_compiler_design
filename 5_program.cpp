b216a2f5af9e592b7728df132ab4d0b5b483d90d
archit669 <architjain669@gmail.com>
2023-09-11 16:16:41 +0530

    add program 4, 5,6

---
+++ 5_program.cpp
@@ -0,0 +1,25 @@
+// write a program whether a string is a constant or not
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    bool isConstant = true; // Use a more descriptive variable name
+
+    for (char c : s) { // Use a range-based for loop for clarity
+        if (!isdigit(c)) { // Use isdigit() to check if a character is a digit
+            isConstant = false;
+            break; // Break out of the loop as soon as a non-digit character is found
+        }
+    }
+
+    if (isConstant) {
+        cout << "Given string is a constant" << endl;
+    } else {
+        cout << "Given string is not a constant" << endl;
+    }
+
+    return 0;
+}
