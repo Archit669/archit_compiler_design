73ac9bbcd4041be5477e3827674e8b7c323ac093
archit669 <architjain669@gmail.com>
2023-12-13 08:19:38 +0530

    update program 9

--- 9_program.cpp
+++ 9_program.cpp
@@ -1,82 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Define the set of terminals and non-terminals
-const char terminals[] = "ab";
-const char nonTerminals[] = "S";
-
-
-// Production rules
-const char *productionRules[] = {
-    "S -> aSb",
-    "S -> ε" // ε represents an empty string
-};
-
-
-// Check if a given string belongs to the grammar
-int belongsToGrammar(const char *input)
-{
-    char parseStack[1000];
-    int top = 0;
-    parseStack[top++] = '$'; // Start symbol
-    parseStack[top++] = 'S'; // Initial state
-    int inputIndex = 0;
-    int valid = 1;
-    while (top > 0 && inputIndex < strlen(input) && valid)
-    {
-        char topStack = parseStack[top - 1];
-        char currentInput = input[inputIndex];
-        if (topStack == currentInput)
-        {
-            top--;
-            inputIndex++;
-        }
-        else
-        {
-            int found = 0;
-            for (int i = 0; i < sizeof(productionRules) / sizeof(productionRules[0]); i++)
-            {
-                char lhs = productionRules[i][0];
-                const char *rhs = productionRules[i] + 4;
-                if (topStack == lhs)
-                {
-                    top--;
-                    if (strcmp(rhs, "ε") != 0)
-                    {
-                        for (int j = strlen(rhs) - 1; j >= 0; j--)
-                        {
-                            parseStack[top++] = rhs[j];
-                        }
-                    }
-                    found = 1;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                valid = 0;
-                break;
-            }
-        }
-    }
-    return valid && top == 0 && inputIndex == strlen(input);
-}
-
+#include<iostream>
+using namespace std;
 
 int main()
-{
-    char input[1000];
-    printf("Enter a string: ");
-    scanf("%s", input);
-    int isValid = belongsToGrammar(input);
-    if (isValid)
-    {
-        printf("The string belongs to the grammar.\n");
-    }
-    else
-    {
-        printf("The string does not belong to the grammar.\n");
-    }
-    return 0;
+{	char str[10];
+	cout<<"\nThe grammar is as follows:\nS -> aS\nS -> Sb\nS -> ab\n";
+	cout<<"Enter a string: ";
+	cin>>str;
+	if(str[0]!='a')
+	{	cout<<"String is invalid because of incorrect first character";
+		exit(0);                                    }
+	int n=1;
+	while(str[n]=='a')  	n++;
+	if ( str[n] != 'b')
+	{	cout<<"string does not belong to grammar";
+		exit(0);	}
+	n++;
+	while (str[n]=='b')      n++;
+	if (str [n] != '\0')
+	{	cout<<"String does not belong to grammar";
+		exit(0);}
+
+	cout<<"string is valid";
 }
